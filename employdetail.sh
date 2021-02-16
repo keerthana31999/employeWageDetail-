1b795e27d088169e9919f8f9f49c94d39d5a7d2f
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 14:01:41 +0530

    Added:employe attendence

--- employdetail.sh
+++ employdetail.sh
@@ -1,1 +1,13 @@
-# welcome to employedatail program
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+isPresent=1
+
+case $empCheck in
+	$isPresent)
+		echo Employee is Present
+		;;
+	*)
+		echo Employee is Absent
+		;;
+esac
