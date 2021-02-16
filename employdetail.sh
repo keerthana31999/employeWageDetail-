e95f74825e7379e9fc53bceef76b8cca4c199f96
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 16:51:05 +0530

    ADDED::hour function

--- employdetail.sh
+++ employdetail.sh
@@ -2,20 +2,17 @@
 #constant
 EMP_RATE_PER_HR=20
 DAYS_IN_MONTH=20
+MAX_HRS_IN_MONTH=100
 
 
-empCheck=$((RANDOM%3))
+
 isPresentfulltime=1
 isPresentPartTime=2
-empMonthWage=0
 days=0
 hours=0
+function getWorkHr() {
+		local empCheck=$1
 
-
-
-while [[ $days -ne 20 && $hours -le 100 ]]
-do
-		empCheck=$((RANDOM%3))
 		if [ $empCheck -eq $isPresentFullTime ]
 		then
 				empHr=8
@@ -25,11 +22,16 @@ do
 		else
 				empHr=0
 		fi
-		hours=$(($hours+$empHr))
+		echo $empHr
+}
+while [[ $days -ne $DAYS_IN_MONTH && $hours -le $MAX_HRS_IN_MONTH ]]
+do
 		((days++))
+		empCheck=$((RANDOM%3))
+		empHr="$( getWorkHr $empCheck)"
+		hours=$(($hours+$empHr))
 
-		empMonthWage=$(($empMonthWage+$empHr*$EMP_RATE_PER_HR))
 done
-
+empMonthWage=$(($hours*$EMP_RATE_PER_HR))
 echo $empMonthWage
 
