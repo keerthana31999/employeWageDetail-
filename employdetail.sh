b0427c39e4f720ad04c8758bf3c81efd72b6f393
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 16:33:25 +0530

    Added::monthly wage

--- employdetail.sh
+++ employdetail.sh
@@ -1,22 +1,34 @@
 #!/bin/bash -x
 #constant
 EMP_RATE_PER_HR=20
+DAYS_IN_MONTH=20
 
 
 empCheck=$((RANDOM%3))
 isPresentfulltime=1
 isPresentPartTime=2
+empMonthWage=0
 
 
-if [ $empCheck -eq $isPresentFullTime ]
-then
-		WORK_HR=8
-elif [ $empCheck -eq $isPresentPartTime ]
-then
-		WORK_HR=4
-else
-		WORK_HR=0
-		echo Employee is Absent
-fi
-salary=$(($WORK_HR*$EMP_RATE_PER_HR))
-echo $salary
+days=0
+
+while [[ $days -ne 20 ]]
+do
+		empCheck=$((RANDOM%3))
+		if [ $empCheck -eq $isPresentFullTime ]
+		then
+				empHr=8
+		elif [ $empCheck -eq $isPresentPartTime ]
+		then
+				empHr=4
+		else
+				empHr=0
+		fi
+
+		((days++))
+
+		empMonthWage=$(($empMonthWage+$empHr*$EMP_RATE_PER_HR))
+done
+
+echo $empMonthWage
+
