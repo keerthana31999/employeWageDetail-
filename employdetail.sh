c55673ecd0f47835ef6635ba700dc40f99ba9071
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 14:12:13 +0530

    Added:employe wage

--- employdetail.sh
+++ employdetail.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+#constant
+EMP_RATE_PER_HR=20
+WORK_HR=8
 
 empCheck=$((RANDOM%2))
 isPresent=1
@@ -6,6 +9,8 @@ isPresent=1
 case $empCheck in
 	$isPresent)
 		echo Employee is Present
+                salary=$(($WORK_HR*$EMP_RATE_PER_HR))
+		echo $salary
 		;;
 	*)
 		echo Employee is Absent
