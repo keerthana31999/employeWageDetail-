228364b63e7ca53af443b1382361e99295ec03cf
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 16:40:57 +0530

    Added::calculation of wage day by day

--- employdetail.sh
+++ employdetail.sh
@@ -8,11 +8,12 @@ empCheck=$((RANDOM%3))
 isPresentfulltime=1
 isPresentPartTime=2
 empMonthWage=0
-
-
 days=0
+hours=0
 
-while [[ $days -ne 20 ]]
+
+
+while [[ $days -ne 20 && $hours -le 100 ]]
 do
 		empCheck=$((RANDOM%3))
 		if [ $empCheck -eq $isPresentFullTime ]
@@ -24,7 +25,7 @@ do
 		else
 				empHr=0
 		fi
-
+		hours=$(($hours+$empHr))
 		((days++))
 
 		empMonthWage=$(($empMonthWage+$empHr*$EMP_RATE_PER_HR))
